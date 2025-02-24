efc00725a48df308a1976f2fe1ee9be49c178406
ztoufik <toufikouedtlelat@gmail.com>
2025-02-24 11:32:36 +0100

    memory location enum

--- src/vm/memloc.rs
+++ src/vm/memloc.rs
@@ -1,1 +1,33 @@
-pub struct MemLoc;
+#[derive(Debug, Clone, PartialEq)]
+pub enum MemLoc {
+    Const(String),
+    Var(String),
+}
+
+impl MemLoc {
+    pub fn memloc_const(value: &str) -> MemLoc {
+        MemLoc::Const(value.to_owned())
+    }
+
+    pub fn memloc_var(value: &str) -> MemLoc {
+        MemLoc::Var(value.to_owned())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::vm::memloc::*;
+
+    #[test]
+    fn memloc_const() {
+        let variable=MemLoc::Const("const".to_owned());
+        assert_eq!(variable, MemLoc::Const("const".to_owned()));
+    }
+
+    #[test]
+    fn memloc_var() {
+        let variable=MemLoc::Var("var".to_owned());
+        assert_eq!(variable, MemLoc::Var("var".to_owned()));
+    }
+
+}
