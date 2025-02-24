84cb6d08f45971f9abf4d5bc5aabba4bca7c6c1f
ztoufik <toufikouedtlelat@gmail.com>
2025-02-24 10:52:48 +0100

    unit test

--- src/vm/vobj.rs
+++ src/vm/vobj.rs
@@ -1,27 +1,55 @@
 #[derive(Debug, Clone, PartialEq)]
-pub enum Vobj<'a> {
-    Str(&'a str),
+pub enum Vobj {
+    Str(String),
     Double(f64),
     Int(i64),
     Null,
 }
 
-impl<'a> Vobj<'a> {
-    pub fn new_str(value: &'a str) -> Vobj {
-        Vobj::Str(value)
+impl Vobj {
+    pub fn new_str(value: &str) -> Vobj {
+        Vobj::Str(value.to_owned())
     }
 
-    pub fn new_double(value: f64) -> Vobj<'a> {
+    pub fn new_double(value: f64) -> Vobj {
         Vobj::Double(value)
     }
 
-    pub fn new_int(value: i64) -> Vobj<'a> {
+    pub fn new_int(value: i64) -> Vobj {
         Vobj::Int(value)
     }
 }
 
-impl<'a> Default for Vobj<'a> {
+impl Default for Vobj {
     fn default() -> Self {
         Vobj::Null
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use crate::vm::Vobj;
+
+    #[test]
+    fn vobj_string() {
+        let variable=Vobj::new_str("test_string");
+        assert_eq!(variable, Vobj::Str("test_string".to_string()));
+    }
+
+    #[test]
+    fn vobj_double() {
+        let variable=Vobj::new_double(4f64);
+        assert_eq!(variable, Vobj::Double(4f64));
+    }
+    
+    #[test]
+    fn vobj_int() {
+        let variable=Vobj::new_int(4i64);
+        assert_eq!(variable, Vobj::Int(4i64));
+    }
+
+    #[test]
+    fn vobj_default() {
+        assert_eq!(Vobj::Null, Vobj::default());
     }
 }
