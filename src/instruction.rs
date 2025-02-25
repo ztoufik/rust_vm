2000b70c6afe262610b865e9d71ceaa85ab2bfaf
ztoufik <toufikouedtlelat@gmail.com>
2025-02-25 18:23:01 +0100

    instruction class + convert &str back to String in MemLoc

--- src/instruction.rs
+++ src/instruction.rs
@@ -3,11 +3,62 @@ use super::memloc::MemLoc;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Instruction<'a> {
-    Load(Vec<MemLoc<'a>>, &'a str, Source<'a>),
+    Load {
+        operand1: MemLoc,
+        operand2: Option<MemLoc>,
+        str_repr: String,
+        src: Source<'a>,
+    },
 }
 
 impl<'a> Instruction<'a> {
-    pub fn load_instruction(operands: Vec<MemLoc<'a>>, str_repr: &'a str, src: Source<'a>) -> Self {
-        Self::Load(operands, str_repr, src)
+    pub fn load_instruction(operand1: MemLoc, operand2: Option<MemLoc>, src: Source<'a>) -> Self {
+        let var1: &str = operand1.ident();
+        let mut str_repr: String = format!("Load {}", var1);
+        if let Some(ref memeloc) = operand2 {
+            str_repr = format!("{} {}", str_repr, memeloc)
+        }
+        Self::Load {
+            operand1,
+            operand2,
+            str_repr,
+            src,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::instruction::*;
+
+    #[test]
+    fn instruction_type_test() {
+        let operand1 = MemLoc::memloc_var("var1".to_owned());
+        let operand2 = Option::None;
+        let src = Source::new("", 0);
+        let variable = Instruction::load_instruction(operand1, operand2, src);
+        assert!(matches!(variable,Instruction::Load {..}));
+    }
+
+    #[test]
+    fn instruction_str_repr_test1() {
+        let operand1 = MemLoc::memloc_var("var1".to_owned());
+        let operand2 = Option::None;
+        let src = Source::new("", 0);
+        let variable = Instruction::load_instruction(operand1, operand2, src);
+        if let Instruction::Load { str_repr, .. } = variable {
+        assert_eq!(str_repr, "Load var1"); // Compare `str_repr` with the expected value
+    }
+    }
+
+    #[test]
+    fn instruction_str_repr_test2() {
+        let operand1 = MemLoc::memloc_var("var1".to_owned());
+        let operand2 = Option::Some(MemLoc::memloc_var("var2".to_owned()));
+        let src = Source::new("", 0);
+        let variable = Instruction::load_instruction(operand1, operand2, src);
+        if let Instruction::Load { str_repr, .. } = variable {
+        assert_eq!(str_repr, "Load var1 var2"); // Compare `str_repr` with the expected value
+    }
     }
 }
--- src/memloc.rs
+++ src/memloc.rs
@@ -1,33 +1,48 @@
+use std::fmt;
 #[derive(Debug, Clone, PartialEq)]
-pub enum MemLoc<'a> {
-    Const(&'a str),
-    Var(&'a str),
+pub enum MemLoc {
+    Const(String),
+    Var(String),
 }
 
-impl<'a> MemLoc<'a> {
-    pub fn memloc_const(value: &'a str) -> Self {
+impl MemLoc {
+    pub fn memloc_const(value:String) -> Self {
         MemLoc::Const(value)
     }
 
-    pub fn memloc_var(value: &'a str) -> Self {
+    pub fn memloc_var(value:String) -> Self {
         MemLoc::Var(value)
+    }
+
+
+    pub fn ident(&self)->&str{
+        match self {
+            MemLoc::Const(value)=> value.as_str(),
+            MemLoc::Var(value)=> value.as_str(),
+        }
     }
 }
 
+impl fmt::Display for MemLoc {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Write the formatted string to the formatter
+        write!(f, "{}", self.ident())
+    }
+}
 #[cfg(test)]
 mod test {
     use crate::memloc::*;
 
     #[test]
     fn memloc_const() {
-        let variable=MemLoc::Const("const");
-        assert_eq!(variable, MemLoc::Const("const"));
+        let variable=MemLoc::Const("const".to_owned());
+        assert_eq!(variable, MemLoc::Const("const".to_owned()));
     }
 
     #[test]
     fn memloc_var() {
-        let variable=MemLoc::Var("var");
-        assert_eq!(variable, MemLoc::Var("var"));
+        let variable=MemLoc::Var("var".to_owned());
+        assert_eq!(variable, MemLoc::Var("var".to_owned()));
     }
 
 }
