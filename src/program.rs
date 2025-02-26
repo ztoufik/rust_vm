76f4a2556fad7c676899fd65d511b1f8926fac8e
ztoufik <toufikouedtlelat@gmail.com>
2025-02-26 07:35:46 +0100

    remove program.rs and add code & consts to Vm

--- src/program.rs
+++
@@ -1,8 +0,0 @@
-use super::vobj::Vobj;
-use super::instruction::Instruction;
-
-#[derive(Debug,Clone,PartialEq)]
-pub struct Program<'a> {
-    consts: Vec<Vobj<'a>>,
-    code: Vec<Instruction<'a>>,
-}
--- src/vm.rs
+++ src/vm.rs
@@ -1,7 +1,10 @@
  
 use super::vobj::Vobj;
+use super::instruction::Instruction;
 
 pub struct Vm<'a> {
     vars: Vec<Vobj<'a>>,
+    consts: Vec<Vobj<'a>>,
     accu: Vobj<'a>,
+    code: Vec<Instruction<'a>>,
 }
