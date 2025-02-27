b6472d3c27ad9589e901d025e63128cca9777164
ztoufik <toufikouedtlelat@gmail.com>
2025-02-27 09:54:29 +0100

    replace generic load instruction with specialised 4 load instruction (memory -> memory) (constant -> memory) (accumlator -> memory) (memory+constant -> accumlator)

--- src/instruction.rs
+++ src/instruction.rs
@@ -3,62 +3,77 @@ use super::memloc::MemLoc;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Instruction<'a> {
-    Load {
+    LoadVV {
         operand1: MemLoc,
-        operand2: Option<MemLoc>,
+        operand2: MemLoc,
+        str_repr: String,
+        src: Source<'a>,
+    },
+
+    LoadCV {
+        operand1: MemLoc,
+        operand2: MemLoc,
+        str_repr: String,
+        src: Source<'a>,
+    },
+
+    LoadWV {
+        operand1: MemLoc,
+        str_repr: String,
+        src: Source<'a>,
+    },
+
+    LoadW {
+        operand1: MemLoc,
         str_repr: String,
         src: Source<'a>,
     },
 }
 
 impl<'a> Instruction<'a> {
-    pub fn load_instruction(operand1: MemLoc, operand2: Option<MemLoc>, src: Source<'a>) -> Self {
+
+    pub fn loadVV_instruction(operand1: MemLoc, operand2: MemLoc, src: Source<'a>) -> Self {
         let var1: &str = operand1.ident();
-        let mut str_repr: String = format!("Load {}", var1);
-        if let Some(ref memeloc) = operand2 {
-            str_repr = format!("{} {}", str_repr, memeloc)
-        }
-        Self::Load {
+        let var2: &str = operand2.ident();
+        let str_repr: String = format!("LoadVV {} {}", var1,var2);
+        Self::LoadVV {
             operand1,
             operand2,
             str_repr,
             src,
         }
     }
-}
 
-#[cfg(test)]
-mod test {
-    use crate::instruction::*;
-
-    #[test]
-    fn instruction_type_test() {
-        let operand1 = MemLoc::memloc_var("var1".to_owned());
-        let operand2 = Option::None;
-        let src = Source::new("", 0);
-        let variable = Instruction::load_instruction(operand1, operand2, src);
-        assert!(matches!(variable,Instruction::Load {..}));
+    pub fn loadCV_instruction(operand1: MemLoc, operand2: MemLoc, src: Source<'a>) -> Self {
+        let const1: &str = operand1.ident();
+        let var2: &str = operand2.ident();
+        let str_repr: String = format!("LoadCV {} {}", const1,var2);
+        Self::LoadCV {
+            operand1,
+            operand2,
+            str_repr,
+            src,
+        }
     }
 
-    #[test]
-    fn instruction_str_repr_test1() {
-        let operand1 = MemLoc::memloc_var("var1".to_owned());
-        let operand2 = Option::None;
-        let src = Source::new("", 0);
-        let variable = Instruction::load_instruction(operand1, operand2, src);
-        if let Instruction::Load { str_repr, .. } = variable {
-        assert_eq!(str_repr, "Load var1"); // Compare `str_repr` with the expected value
-    }
+    pub fn loadWV_instruction(operand1: MemLoc, src: Source<'a>) -> Self {
+        let var1: &str = operand1.ident();
+        let str_repr: String = format!("LoadWV {}", var1);
+        Self::LoadWV {
+            operand1,
+            str_repr,
+            src,
+        }
     }
 
-    #[test]
-    fn instruction_str_repr_test2() {
-        let operand1 = MemLoc::memloc_var("var1".to_owned());
-        let operand2 = Option::Some(MemLoc::memloc_var("var2".to_owned()));
-        let src = Source::new("", 0);
-        let variable = Instruction::load_instruction(operand1, operand2, src);
-        if let Instruction::Load { str_repr, .. } = variable {
-        assert_eq!(str_repr, "Load var1 var2"); // Compare `str_repr` with the expected value
+    pub fn loadW_instruction(operand1: MemLoc, src: Source<'a>) -> Self {
+        let var1: &str = operand1.ident();
+        let str_repr: String = format!("LoadW {}", var1);
+        Self::LoadW {
+            operand1,
+            str_repr,
+            src,
+        }
     }
-    }
+
 }
