373b26f40e275852895755d12c52ea09e3c04ed4
ztoufik <toufikouedtlelat@gmail.com>
2025-02-27 09:52:17 +0100

    display trait for source

--- src/err.rs
+++ src/err.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 #[derive(Debug, Clone, PartialEq)]
 pub struct Source<'a> {
     source_file: &'a str,
@@ -8,4 +9,11 @@ impl<'a> Source<'a>{
    pub fn new(source_file:&'a str,line_number:u32)->Self{
     Self { source_file, line_number }
    }
+}
+
+impl<'a> fmt::Display for Source<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Write the formatted string to the formatter
+        write!(f, "{}:{}", self.source_file,self.line_number)
+    }
 }
